7d748ae6d31a3969a018cda75e66e732a9093359
Adolfo-Kuhn <akuhnbolanos@gmail.com>
2019-03-26 18:12:29 +0100

    UD_Boletín Ejercicio 1

---
+++ U6_Boletin_1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $bob1 = array('TITLE' => 'Empire Burlesque', 'ARTIST' => 'Bob Dylan', 'PRICE' => '10.90', 'YEAR' => '1985');
+            $bonnie1 = array('TITLE' => 'Hide your heart', 'ARTIST' => 'Bonnie Tyler', 'PRICE' => '9.90', 'YEAR' => '1988');
+            $dolly1 = array('TITLE' => 'Greatest Hits', 'ARTIST' => 'Dolly Parton', 'PRICE' => '9.90', 'YEAR' => '1982');
+            $gary1 = array('TITLE' => 'Still got the blues', 'ARTIST' => 'Gary Moore', 'PRICE' => '9.90', 'YEAR' => '1982');
+     
+            $bbdd = array($bob1, $bonnie1, $dolly1, $gary1);
+        ?>
+    </body>
+</html>
